116460c65665d2d2c8f62243ba3eb7c97e32db19
onxmoreplz <hamdoyoung95@gmail.com>
2021-01-07 22:08:07 +0900

    E-mail 로그인 기능 추가

--- AndroidProject/app/src/main/java/com/example/cloneinstagram/LoginActivity.kt
+++ AndroidProject/app/src/main/java/com/example/cloneinstagram/LoginActivity.kt
@@ -1,11 +1,58 @@
 package com.example.cloneinstagram
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_login.setOnClickListener {
+            signInAndSignUp()
+
+        }
     }
+
+    fun signInAndSignUp() {
+        auth?.createUserWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    //계정이 있는 경우
+                    signInEmail()
+                }
+            }
+    }
+
+    fun signInEmail() {
+        auth?.createUserWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {//로그인 성공
+                    moveMainPage(task.result?.user)
+                } else {//로그인 실
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
 }
