3a85154f70a1387890eec809afaa6695fea2c1b0
onxmoreplz <hamdoyoung95@gmail.com>
2021-01-10 21:57:12 +0900

    사진업로드 시 외부 Storage 권한 획득 코드 추가

--- AndroidProject/app/src/main/java/com/example/cloneinstagram/MainActivity.kt
+++ AndroidProject/app/src/main/java/com/example/cloneinstagram/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.cloneinstagram
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.cloneinstagram.navigation.AddPhotoActivity
 import com.example.cloneinstagram.navigation.AlarmFragment
 import com.example.cloneinstagram.navigation.DetailViewFragment
 import com.example.cloneinstagram.navigation.GridFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -29,7 +37,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
-
+                //외부 Storage 권한 여부 확인
+                if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) ==  PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm -> {
