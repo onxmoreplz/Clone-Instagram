7e5d01a3c94ffba8271af7d7e21761688eed3230
onxmoreplz <hamdoyoung95@gmail.com>
2021-01-13 23:55:50 +0900

    content DTO 클래스 생성

---
+++ AndroidProject/app/src/main/java/com/example/cloneinstagram/navigation/model/contentDTO.kt
@@ -0,0 +1,18 @@
+package com.example.cloneinstagram.navigation.model
+
+data class ContentDTO(
+    var explain: String? = null,
+    var imageUrl: String? = null,
+    var uid: String? = null,
+    var userId: String? = null,
+    var timeStamp: Long? = null,
+    var favoriteCount: Int = 0,
+    var favorites: Map<String, Boolean> = HashMap()
+) {
+    data class Comment(
+        var uid: String? = null,
+        var userId: String? = null,
+        var comment: String? = null,
+        var timeStamp: Long? = null,
+    )
+}
